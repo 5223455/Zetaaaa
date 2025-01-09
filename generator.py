4224653ca3f433317fbd669213574f1330ef0292
5223455 <144127908+5223455@users.noreply.github.com>
2025-01-09 04:06:05 +0000

    lambda

---
+++ generator.py
@@ -0,0 +1,8 @@
+result = lambda x,y:x+y
+print("Addition of two lambda values:",result(1,2))
+result = lambda x,y:x-y
+print("subtraction of two lambda values:",result(1,2))
+result = lambda x,y:x*y
+print("Multiplication of two lambda values:",result(1,2))
+result = lambda x,y:x/y
+print("Divison of two lambda values:",result(1,2))
